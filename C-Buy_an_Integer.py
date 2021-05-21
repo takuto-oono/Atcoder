1cfedda7d8b0cd7e0729786d50400a78a8d9ce26
takutoono053037 <takutoono053037@gmail.com>
2021-05-21 16:34:25 +0900

    5/21

--- C-Buy_an_Integer.py
+++ C-Buy_an_Integer.py
@@ -1,15 +1,19 @@
 a, b, x = map(int, input().split())
-n_condidate_list = [10 * i for i in range(19)]
-memo = 0
-for i in range(19):
 
-    y = n_condidate_list[i]
-    if a * y + b * (i + 1) > x:
-        memo = y
-        break
-while (a * memo + b * len(str(memo)) > x):
-    memo -= 1
-if memo < 0:
-    memo = 0
-print(memo)
+ok = 0
+ng = 10 ** 9 + 1
+
+def f(x, a, b):
+    return a * x + b * len(str(x))
+
+while(ok + 1 != ng):
+
+    y = (ok + ng) // 2
+    if f(y, a, b) <= x:
+        ok = y
+    else:
+        ng = y
+print(ok)
+
+
 
