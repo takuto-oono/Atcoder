c897097534b26d54a804faaa4f701aad4d10ef02
takutoono053037 <takutoono053037@gmail.com>
2021-06-18 16:50:42 +0900

    6/18

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-import mathimport statisticsn, k = map(int, input().split())A = []for i in range(n):    a = list(map(int, input().split()))    A.append(a)ans = 10 ** 10for i in range(n - k + 1):    for j in range(n - k + 1):        B = []        for y in range(i, i + k):            for x in range(j, j + k):                B.append(A[y][x])        if n % 2 == 1:            median = statistics.median(B)        else:            median = statistics.median_low(B)        ans = min(median, ans)print(ans)
+x = 0.04n = 1 + x / 100print(9000 * 36 * n ** 365 - 9000 * 12)
