1c9d96d0465df06d4a7c895cab108209e03c751a
TO053037 <67772806+TO053037@users.noreply.github.com>
2021-10-04 15:17:40 +0900

    10/4

--- D-Online_games.py
+++ D-Online_games.py
@@ -1,63 +1,37 @@
+from collections import Counter
 
 
 
 
-
-
-def Entry(A, Online_people):
-    A.sort()
-    n = len(A)
-    for i in range(n):
-        Online_people[i] = 10 ** 100 - A[i]
-    
-
-    return Online_people
-
-def Leaving(B, Online_people):
-    B.sort(reverse=True)
-    n = len(B)
-    for i in range(n):
-        
-        b = 10 ** 100 - B[i]
-        Online_people[i] = Online_people[i] - b
-        
-        
-    
-    return Online_people
-
-def Create_ans(Online_people):
-    Ans = list(reversed(Online_people))
-    
-    n = len(Online_people)
-    c = 0
-    for i in range(n):
-        Ans[i] = Ans[i] - c
-        c += Ans[i]
-    
-    Ans = list(reversed(Ans))
-    for i in range(n):
-        Ans[i] = str(Ans[i])
-
-    print(' '.join(Ans))
-        
 
 def main():
     n = int(input())
-    A = []
-    B = []
-    for i in range(n):
+    C = Counter()
+    
+    for _ in range(n):
         a, b = map(int, input().split())
-        A.append(a)
-        B.append(a + b)
+
+        C[a] += 1
+        C[a + b] -= 1
     
-    Online_people = [0 for _ in range(n)]
     
-    Online_people = Entry(A, Online_people)
+    ans = [0 for _ in range(n + 1)]
+    days = sorted(C.keys())
+    pre_day = 0
+    count = 0
 
-    Online_people = Leaving(B, Online_people)
+    for day in days:
+        ans[count] += day - pre_day
+        count += C[day]
+        pre_day = day
+    
+    print(*ans[1:])
 
-    Create_ans(Online_people)
+
+        
 
 
-main()
+if __name__ == '__main__':
+    main()
+
 
