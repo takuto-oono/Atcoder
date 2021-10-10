9ead0e944b0e6ea7b99ac3c824f6a1ac3e43d0ba
TO053037 <67772806+TO053037@users.noreply.github.com>
2021-10-10 14:25:57 +0900

    10/10

--- D-Line.py
+++ D-Line.py
@@ -1,40 +1,32 @@
-import networkx as nx
 
 
 
 
 
-def create_graph(n, x, y):
-    graph = nx.Graph()
-    nx.add_path(graph, [i for i in range(1, n + 1)])
-    nx.add_path(graph, [x, y])
 
-    return graph
+def create_distance(i, j, x, y):
+    return min(abs(j - i), abs(x - i) + abs(y - j) + 1)
 
-def insearch_distance(graph, n):
-    ans = [0 for i in range(n - 1)]
-    for i in range(1, n):
-        for j in range(i + 1, n + 1):
-            if (nx.has_path(graph, source=i, target=j)):
-                distance = len(nx.shortest_path(graph, source=i, target=j)) - 1
-                ans[distance - 1] += 1
-    
-    return ans
-
-def print_ans(ans):
-    for a in ans:
-        print(a)
-                
-
+def print_ans(ans_list):
+    for ans in ans_list:
+        print(ans)
 
 def main():
     n, x, y = map(int, input().split())
-    graph = create_graph(n, x, y)
-    ans = insearch_distance(graph, n)
-    print_ans(ans)
+    ans_list = [0 for _ in range(n - 1)]
     
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            distance = create_distance(i + 1, j + 1, x, y)
+            ans_list[distance - 1] += 1
+
+    print_ans(ans_list)
 
 if __name__ == '__main__':
     main()
 
 
+
+        
+
+        
