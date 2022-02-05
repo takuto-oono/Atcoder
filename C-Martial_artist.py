f136759561c9c89dabe0fa5689e2d519bc8ec6f8
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-02-05 14:10:55 +0900

    2022/2/5

--- C-Martial_artist.py
+++ C-Martial_artist.py
@@ -1,16 +1,24 @@
+
+
 def main():
     n = int(input())
     t_list = []
-    skill_list = []
+    skill_branch = [[] for _ in range(n - 1)]
     for i in range(n):
         l = list(map(int, input().split()))
         t = l.pop(0)
         t_list.append(t)
-        skill_list.append(l)
+        r = l.pop(0)
+        
+        for j in range(r):
+            skill_branch[l[j] - 1].append(i)
+    
+    def search(now, goal, todo_list):
+        
+    
+            
 
     
-    
-
 
 if __name__ == '__main__':
     main()
