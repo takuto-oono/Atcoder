0c31fc309c248b02fa21d08486e99cd89ab4da64
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-02-10 15:37:39 +0900

    tomorrow

--- D-Neighbors.py
+++ D-Neighbors.py
@@ -1,74 +1,66 @@
+def dfs(n, branch, start, seen_list):
+    todo_list = []
+    if seen_list[start] == False:
+        seen_list[start] = True
+        todo_list.append(start)
+
+    while(len(todo_list) > 0):
+        todo = todo_list.pop(0)
+        for b in branch[todo]:
+            if seen_list[b]:
+                return False
+
+            seen_list[b] = True
+            todo_list.append(b)
+
+    return True
 
 
+def judge(n, branch, ab_list):
+    max_len = 0
+    for b in branch:
+        max_len = max(max_len, len(b))
 
+    if max_len > 2:
+        return False
 
+    seen_list = [False for _ in range(n)]
+    for start in ab_list:
+        judgement = dfs(n, branch, start, seen_list)
+        print(seen_list)
+        if judgement:
+            continue
 
+        return False
 
-def main():
-    n, m = map(int, input().split())
-    ab_list = []
-    ab_dic = {}
-    for i in range(m):
-        a, b = map(int, input().split())
-        ab_list.append([a, b])
-        
-    
-    ab_count = [0 for _ in range(n)]
-    
-    for i in range(m):
-        a = ab_list[i][0]
-        b = ab_list[i][1]
-        ab_count[a - 1] += 1
-        ab_count[b - 1] += 1
-    
-    for i in range(n):
-        if ab_count[i] >= 3:
-            print('No')
-            exit()
-    
-    memo = [0 for i in range(n)]
-    branch_list = []
-    for i in range(m):
-        a = ab_list[i][0]
-        b = ab_list[i][1]
-        
-        for j in range(len(branch_list)):
-            branch = branch_list[j]
-            if (branch[0] == a and branch[1] == b) or (branch[1] == a and branch[0] == b):
-                print('No')
-                exit()
-            
-            if branch[0] == a:
-                branch[0] = b
-                continue
-            
-            if branch[0] == b:
-                branch[0] = a
-                continue
-            
-            if branch[1] == a:
-                branch[1] = b
-                continue
-            
-            if branch[1] == b:
-                branch[1] = a
-                continue
-            
-            
-            
-        
-            
-            
-            
-        
-        
-        
-        
-    
-    
+    return True
+
 
 if __name__ == '__main__':
-    main()
-        
-        
-    
+    n, m = map(int, input().split())
+    branch = [[] for _ in range(n)]
+    ab_list = []
+    count_list = [0 for _ in range(n)]
+    for i in range(m):
+        a, b = map(int, input().split())
+        a -= 1
+        b -= 1
+        count_list[a] += 1
+        count_list[b] += 1
+
+        branch[a].append(b)
+        branch[b].append(a)
+        ab_list.append(a)
+        ab_list.append(b)
+
+    if max(count_list) > 2:
+        print('No')
+        exit()
+
+    ab_list = list(set(ab_list))
+
+    if judge(n, branch, ab_list):
+        print('Yes')
+
+    else:
+        print('No')
