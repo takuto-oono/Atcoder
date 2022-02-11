aee66a7552e5e724b281d75a6112df629f5dfb84
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-02-11 19:09:03 +0900

    fix

--- C-Martial_artist.py
+++ C-Martial_artist.py
@@ -1,23 +1,33 @@
+def create_is_used_list(skill_list, n):
+    is_used_list = [False for _ in range(n)]
+    is_used_list[n - 1] = True
+
+    for i, skills in enumerate(reversed(skill_list)):
+        if is_used_list[n - i - 1]:
+            for skill in skills:
+                is_used_list[skill - 1] = True
+
+    return is_used_list
 
 
-def main():
-    n = int(input())
-    t_list = []
-    skill_branch = [[] for _ in range(n - 1)]
-    for i in range(n):
-        l = list(map(int, input().split()))
-        t = l.pop(0)
-        t_list.append(t)
-        r = l.pop(0)
-        
-        for j in range(r):
-            skill_branch[l[j] - 1].append(i)
-    
-        
-    
-            
+def cal_total_time(is_used_list, time_list):
+    ans = 0
+    for i, is_used in enumerate(is_used_list):
+        if is_used:
+            ans += time_list[i]
 
-    
+    print(ans)
+
 
 if __name__ == '__main__':
-    main()
+    n = int(input())
+    t_list = []
+    a_list = []
+    for i in range(n):
+        tka = list(map(int, input().split()))
+        t = tka.pop(0)
+        k = tka.pop(0)
+        t_list.append(t)
+        a_list.append(tka)
+
+    cal_total_time(create_is_used_list(a_list, n), t_list)
