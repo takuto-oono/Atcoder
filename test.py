d463f55fa3ceda1efeddf881ec65b3f2e37400fc
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-02-13 17:10:15 +0900

    complete

--- test.py
+++ test.py
@@ -1,10 +1,40 @@
-print('Hello_Python')
+import os
 
+test_file = 'test_test.py'
 
-def add_0(_id):
-    _id = '0' * (6 - len(_id)) + _id
-    return _id
+n = 4
 
+def create_file_name(name, index):
+    return name + str(index + 1) + '.txt'
 
-print(add_0('45'))
-print(add_0('111111'))
+def run():
+    for i in range(n):
+        input_file = create_file_name('input', i)
+        output_file = create_file_name('output', i)
+        command = 'python3 ' + test_file
+        os.system("python3 " + test_file + ' < ' + input_file + ' > ' + output_file)
+    
+
+def read_file(file_name):
+    f = open(file_name, 'r')
+    data = f.read()
+    f.close()
+    return data
+
+def judge():
+    for i in range(n):
+        output_file = create_file_name('output', i)
+        ans_file = create_file_name('ans', i)
+        
+        output_data = read_file(output_file)
+        ans_data = read_file(ans_file)
+        
+        if output_data == ans_data:
+            print('test case' + str(i + 1) + ' True')
+        else:
+            print('test case' + str(i + 1) + ' False')
+
+if __name__ == '__main__':
+    run()
+    judge()
+    
