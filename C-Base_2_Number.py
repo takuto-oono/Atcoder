417e8726a356c186a8803901502d8f1b0bb81172
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-02-20 13:17:23 +0900

    complete

--- C-Base_2_Number.py
+++ C-Base_2_Number.py
@@ -1,23 +1,13 @@
-def base_conversion(n: int) -> None:
-    i = 0
-    ans = []
-    
-    while():
-        x = 2 ** (i + 1)
-        if abs(n) % x == 1:
-            ans.append('1')
-            
-        else:
-            ans.append('0')
-        
-        print(n)
-        i += 1
-    ans.reverse()
-    print(n)
-    print("".join(ans))
-    
 if __name__ == '__main__':
     n = int(input())
-    # print(abs(-9 % -8))
-    base_conversion(n)
-    
+    ans = ''
+    if n != 0:
+        while(n != 0):
+            memo = n % 2
+            ans += str(memo)
+            n -= memo
+            n //= -2
+    else:
+        ans = '0'
+
+    print(''.join(ans[::-1]))
