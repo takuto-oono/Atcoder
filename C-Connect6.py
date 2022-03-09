a9e1dca1b596de3c7343ac238d91c50d722c94a4
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-03-09 09:35:17 +0900

    complete

--- C-Connect6.py
+++ C-Connect6.py
@@ -3,41 +3,36 @@ n = 0
 s_list = []
 
 
-def count_black(color_list: List[int]) -> bool:
-    cnt = 0
-    for color in color_list:
-        if color == '#':
-            cnt += 1
-
-    return cnt >= 4
+def count_black(list: List) -> bool:
+    return list.count('#') >= 4
 
 
-def judge():
+def judge() -> bool:
     for y in range(n):
         for x in range(n):
-            if y <= n - 6:
-                if count_black([s_list[y + i][x] for i in range(6)]):
-                    return True
-
-            if x <= n - 6:
-                if count_black([s_list[y][x + i] for i in range(6)]):
-                    return True
-
-            if y <= n - 6 and x <= n - 6:
+            if (y < n - 5) and (x < n - 5):
                 if count_black([s_list[y + i][x + i] for i in range(6)]):
                     return True
 
-            if y <= n - 6 and x < 6:
-                if count_black([s_list[y + i][x - i] for i in range(6)]):
+            if (y < n - 5):
+                if count_black([s_list[y + i][x] for i in range(6)]):
                     return True
+
+            if (x < n - 5):
+                if count_black([s_list[y][x + i] for i in range(6)]):
+                    return True
+
+            if (y >= 5) and (x < n - 5):
+                if count_black([s_list[y - i][x + i] for i in range(6)]):
+                    return True
+
     return False
 
 
 if __name__ == '__main__':
     n = int(input())
     for _ in range(n):
-        s = list(input())
-        s_list.append(s)
+        s_list.append(list(input()))
 
     if judge():
         print('Yes')
