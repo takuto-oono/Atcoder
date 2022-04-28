4e406028aac7eab02431051dffc51a9cac003303
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-04-28 13:07:14 +0900

    change dir

--- opt/test.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hello Docker Python')
-
-
-if __name__ == '__main__':
-    main()
--- test.py
+++ test.py
@@ -1,40 +1,6 @@
-import os
+def main():
+    print('Hello Docker Python')
 
-test_file = 'test_test.py'
-
-n = 4
-
-def create_file_name(name, index):
-    return name + str(index + 1) + '.txt'
-
-def run():
-    for i in range(n):
-        input_file = create_file_name('input', i)
-        output_file = create_file_name('output', i)
-        command = 'python3 ' + test_file
-        os.system("python3 " + test_file + ' < ' + input_file + ' > ' + output_file)
-    
-
-def read_file(file_name):
-    f = open(file_name, 'r')
-    data = f.read()
-    f.close()
-    return data
-
-def judge():
-    for i in range(n):
-        output_file = create_file_name('output', i)
-        ans_file = create_file_name('ans', i)
-        
-        output_data = read_file(output_file)
-        ans_data = read_file(ans_file)
-        
-        if output_data == ans_data:
-            print('test case' + str(i + 1) + ' True')
-        else:
-            print('test case' + str(i + 1) + ' False')
 
 if __name__ == '__main__':
-    run()
-    judge()
-    
+    main()
