a87d3618c150b55e30a522f94052d7e17e2161ce
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-04-28 14:11:25 +0900

    2022/4/28

--- D-Range_Count_Query.py
+++ D-Range_Count_Query.py
@@ -1,45 +1,25 @@
+import bisect
+
+
 def main():
     n = int(input())
     a_list = list(map(int, input().split()))
     a_dic = {}
     for i, a in enumerate(a_list):
         if a in a_dic:
-            a_dic[a].append(i)
+            a_dic[a].append(i + 1)
         else:
-            a_dic[a] = [i]
+            a_dic[a] = [i + 1]
 
     q = int(input())
     for _ in range(q):
         l, r, x = map(int, input().split())
-        ans = 0
-        if x in a_dic:
-            ok1 = 0
-            ng1 = len(a_dic[x]) - 1
+        if x not in a_dic:
+            print(0)
+            continue
 
-            while ng1 - ok1 > 1:
-                mid = (ng1 + ok1) // 2
-                if a_dic[x][mid] == l:
-                    ok1 = mid
-                    break
-                elif a_dic[x][mid] < l:
-                    ok1 = mid
-                else:
-                    ng1 = mid
-
-            ok2 = len(a_dic[x]) - 1
-            ng2 = 0
-
-            while ok2 - ng2 > 1:
-                mid = (ok2 + ng2) // 2
-                if a_dic[x][mid] == r:
-                    ok2 = mid
-                    break
-                elif a_dic[x][mid] > r:
-                    ok2 = mid
-                else:
-                    ng2 = mid
-
-            ans = ok2 - ok1 + 1
+        ans = bisect.bisect_right(
+            a_dic[x], r) - bisect.bisect_left(a_dic[x], l)
         print(ans)
 
 
