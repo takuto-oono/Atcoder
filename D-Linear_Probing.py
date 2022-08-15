056ab2bf42cd4b38ee3439ee9a8a14866a329aa0
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-08-15 20:17:01 +0900

    fix TLE

--- D-Linear_Probing.py
+++ D-Linear_Probing.py
@@ -6,28 +6,23 @@ class LinerProbing:
         for _ in range(self.q):
             self.query_list.append(tuple(map(int, input().split())))
         self.a_dic = {}
-        self.a_dic_keys_list = []
+        self.a_dic_keys_list = [i for i in range(self.n)]
 
     def rewrite_a_list(self, index: int, value: int) -> None:
         self.a_dic[index] = value
-        self.a_dic_keys_list.append(index)
-        self.a_dic_keys_list.sort()
+        self.a_dic_keys_list[index] = self.a_dic_keys_list[(index + 1) % self.n]
+
+    def find_index(self, h: int) -> int:
+        while self.a_dic_keys_list[h] != h:
+            h = self.a_dic_keys_list[h]
+        return h
 
     def process1(self, x: int) -> None:
         h = x % self.n
         if h not in self.a_dic.keys():
             self.rewrite_a_list(h, x)
         else:
-            new_h = self.a_dic_keys_list[-1] + 1
-            is_search = False
-            for i in range(len(self.a_dic_keys_list) - 1):
-                if self.a_dic_keys_list[i] == h:
-                    is_search = True
-                if is_search:
-                    if self.a_dic_keys_list[i + 1] > self.a_dic_keys_list[i] + 1:
-                        new_h = self.a_dic_keys_list[i] + 1
-                        break
-            self.rewrite_a_list(new_h, x)
+            self.rewrite_a_list(self.find_index(h), x)
 
     def process2(self, x: int) -> None:
         x %= self.n
