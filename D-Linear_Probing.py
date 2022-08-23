ae9b31a50654d8160865a2a6e918b4f2d82ccfa6
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-08-23 13:11:21 +0900

    complete

--- D-Linear_Probing.py
+++ D-Linear_Probing.py
@@ -2,37 +2,31 @@ class LinerProbing:
     def __init__(self) -> None:
         self.n = 2 ** 20
         self.q = int(input())
-        self.query_list = []
-        for _ in range(self.q):
-            self.query_list.append(tuple(map(int, input().split())))
-        self.a_dic = {}
-        self.a_dic_keys_list = [i for i in range(self.n)]
+        self.a_list = [-1 for _ in range(self.n)]
+        self.changing_index_list = [i for i in range(self.n)]
 
     def rewrite_a_list(self, index: int, value: int) -> None:
-        self.a_dic[index] = value
-        self.a_dic_keys_list[index] = self.a_dic_keys_list[(index + 1) % self.n]
+        self.a_list[index] = value
+        self.changing_index_list[index] = self.changing_index_list[(index + 1) % self.n]
 
     def find_index(self, h: int) -> int:
-        while self.a_dic_keys_list[h] != h:
-            h = self.a_dic_keys_list[h]
+        rewrite_index_list = []
+        while self.changing_index_list[h] != h:
+            rewrite_index_list.append(h)
+            h = self.changing_index_list[h]
+        for index in rewrite_index_list:
+            self.changing_index_list[index] = h
         return h
 
     def process1(self, x: int) -> None:
-        h = x % self.n
-        if h not in self.a_dic.keys():
-            self.rewrite_a_list(h, x)
-        else:
-            self.rewrite_a_list(self.find_index(h), x)
+        self.rewrite_a_list(self.find_index(x % self.n), x)
 
     def process2(self, x: int) -> None:
-        x %= self.n
-        if x in self.a_dic.keys():
-            print(self.a_dic[x])
-        else:
-            print(-1)
+        print(self.a_list[x % self.n])
 
     def main_process(self):
-        for (t, x) in self.query_list:
+        for _ in range(self.q):
+            t, x = map(int, input().split())
             if t == 1:
                 self.process1(x)
             if t == 2:
